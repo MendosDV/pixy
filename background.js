02a29535bf3c195ee38f5acb8607962ad7e69cd1
Mickael Riss <mendos.prod@gmail.com>
2023-06-07 11:02:37 +0200

    Ajout background content

--- background.js
+++ background.js
@@ -1,10 +1,20 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(messageFromContent = (request, sender, sendResponse) => {
   console.log(request.message);
 });
 
-const URL = [];
-const TITLE = [];
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// Add URL to visitedSites
+const visitedSites = [];
+chrome.tabs.onUpdated.addListener(addUrl= (tabId, changeInfo, tab) => {
+  console.log("updated")
+  if (changeInfo.status === 'complete' && tab.active) {
+    visitedSites.push({title: tab.title, url: tab.url});
+    console.log(visitedSites);
+  }
 
+  chrome.tabs.executeScript({
+    code:  `const text = document.body.innerHTML;
+    const modifiedText = text.replace(/chat/g, "chien");
+    document.body.innerHTML = modifiedText;`
+  });
 });
--- content.js
+++ content.js
@@ -2,4 +2,16 @@ chrome.runtime.sendMessage(chrome.runtime.id, {
   message: "helloBackground"
 })
 
-  console.log("yoContent");
+//   console.log("yoContent");
+
+// const mot = /\bs[a@#$%^&*()+=\[\]{};':"\\|,.<>\/?`~éèêëēĕėęěèéêëēĕėęě]s?/gmi;
+// const replace = "Yoann";
+
+// const body = document.querySelector("body").textContent
+// console.log(body);
+// console.log(body.length);
+
+
+// const text = document.body.innerHTML;
+// const modifiedText = text.replace(/chat/g, "chien");
+// document.body.innerHTML = modifiedText;
