d979f4080913adbda925377163c95949a1646271
Mickael Riss <mendos.prod@gmail.com>
2023-06-09 16:12:07 +0200

    content js

--- content.js
+++ content.js
@@ -1,23 +1,15 @@
-
-
 chrome.runtime.sendMessage({ message: 'executeScript' });
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'updateDOM') {
     const modifiedDOM = request.modifiedDOM;
     const container = document.body;
     container.innerHTML = modifiedDOM;
+
+    let blured = document.querySelectorAll(".blur");
+
+    for(let i = 0; i < blured.length; i++){
+      blured[i].style.color = "white";
+      blured[i].style.backgroundColor = "green";
+    }
   }
 });
-//   console.log("yoContent");
-
-// const mot = /\bs[a@#$%^&*()+=\[\]{};':"\\|,.<>\/?`~éèêëēĕėęěèéêëēĕėęě]s?/gmi;
-// const replace = "Yoann";
-
-// const body = document.querySelector("body").textContent
-// console.log(body);
-// console.log(body.length);
-
-
-// const text = document.body.innerHTML;
-// const modifiedText = text.replace(/chat/g, "chien");
-// document.body.innerHTML = modifiedText;
