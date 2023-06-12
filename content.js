37e5c930462e19e9ff2492c25b492fb14d4c45d8
Mickael Riss <mendos.prod@gmail.com>
2023-06-12 16:58:32 +0200

    le code fonctionne

--- content.js
+++ content.js
@@ -8,19 +8,32 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { /
     container.innerHTML = modifiedDOM; // set modifiedDOM to container (body)
 
     let pixies = document.querySelectorAll("pixy"); // get all pixies
-    let pixyExplication = document.querySelector("pixy-explication") // for each pixy
-    console.log(pixyExplication);
 
-    pixies.forEach(pixy => {
-      pixy.addEventListener("click", function() {
-        let word = pixy.dataset.word;
-        let description = pixy.dataset.description;
-        pixyExplication.innerHTML = description;
-        pixyExplication.style.display = "block";
-        pixyExplication.style.top = pixy.top;
-        pixyExplication.style.left = pixy.left;
+    pixies.forEach((pixy) => {
+      let level = pixy.dataset.level;
+      pixy.style.textDecoration = 'underline orange wavy';
 
-      })
+      if (level === 'low') {
+        pixy.style.color = "purple";
+      }
+      else if (level === 'medium') {
+        pixy.style.color = "blue";
+      }
+      else {
+        pixy.style.color = "yellow";
+      }
     })
+
+    // pixies.forEach(pixy => {
+    //   pixy.addEventListener("click", function() {
+    //     let word = pixy.dataset.word;
+    //     let description = pixy.dataset.description;
+    //     pixyExplication.innerHTML = description;
+    //     pixyExplication.style.display = "block";
+    //     pixyExplication.style.top = pixy.top;
+    //     pixyExplication.style.left = pixy.left;
+
+    //   })
+    // })
   }
 });
