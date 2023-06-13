8b11d4731e18cbaddf976b0c23ca9611671cd7c4
Mickael Riss <mendos.prod@gmail.com>
2023-06-13 10:13:36 +0200

    push

--- content.js
+++ content.js
@@ -1,7 +1,32 @@
 chrome.runtime.sendMessage({ message: 'executeScript' });  // send message to background
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { // listen for message from background
-  let loader = `<div style="width: 100%; position: absolute;" class="boxLoading">Salope !!!!!!!!</div>`
+  // let loader = `<div style="width: 100%; position: absolute;" class="boxLoading">Salope !!!!!!!!</div>`
+  const style = `
+  <style>
+    pixy-explication {
+      width: 20vw;
+      padding: 35px;
+      border: 1px solid rgba(255, 255, 255, .25);
+      border-radius: 8px;
+      background-color: rgba(255, 255, 255, 0.45);
+      box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.25);
+      backdrop-filter: blur(15px);
+    }
+    pixy-word {
+      font-size: 1.2rem;
+      color: rgb(222, 175, 73);
+      padding-bottom: 0.4rem;
+      border-bottom: 1px solid rgb(222, 175, 73);
+    }
+    pixy-description {
+      font-size: 1rem;
+      margin-top: 1.4rem;
+    }
+  </style>
+`;
+
+  document.head.insertAdjacentHTML('beforeend', style);
 
   if (request.action === 'updateDOM') { // if action is updateDOM
     const modifiedDOM = request.modifiedDOM; // get modifiedDOM from request
@@ -24,9 +49,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { /
         pixy.addEventListener("click", function() {
           let pixyExplication = pixy.querySelector("pixy-explication");
           pixyExplication.style.display = "block";
-          pixyExplication.style.backgroundColor = "black";
-          pixyExplication.style.top = pixy.top;
-          pixyExplication.style.left = pixy.left;
         })
       }
       else {
@@ -35,9 +57,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { /
         pixy.addEventListener("click", function() {
           let pixyExplication = pixy.querySelector("pixy-explication");
           pixyExplication.style.display = "block";
-          pixyExplication.style.backgroundColor = "black";
-          pixyExplication.style.top = pixy.top;
-          pixyExplication.style.left = pixy.left;
         })
       }
     });
